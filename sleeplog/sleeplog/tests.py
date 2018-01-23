3245b1223330f9d3ac9c8b8023c293b95d966f28
James Payne <github@jepaynedev.com>
2018-01-22 22:25:37 -0600

    Add webtest and functional test

--- sleeplog/sleeplog/tests.py
+++ sleeplog/sleeplog/tests.py
@@ -17,3 +17,15 @@ class SleepLogViewTests(unittest.TestCase):
         inst = SleepLogViews(request)
         response = inst.home()
         self.assertEqual(b'Placeholder', response.body)
+
+
+class SleepLogFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from sleeplog import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_home(self):
+        res = self.testapp.get('/', status=200)
+        self.assertIn(b'Placeholder', res.body)
