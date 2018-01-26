91731f1e23efb207f176fe2f1204a389e4fa6228
James Payne <github@jepaynedev.com>
2018-01-25 23:10:56 -0600

    Move pyramid_tm inclusion into code config to avoid repitition

--- sleeplog/sleeplog/__init__.py
+++ sleeplog/sleeplog/__init__.py
@@ -15,6 +15,7 @@ def main(global_config, **settings):
         settings=settings,
         root_factory='sleeplog.models.Root'
     )
+    config.include('pyramid_tm')
     config.include('pyramid_jinja2')
     config.add_route('home', '/')
     config.scan('.views')
